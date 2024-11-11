91509f4c159f9c09770111db98382da3f3c1e8c7
Bayu-x3 <rpsmaster12@gmail.com>
2024-11-11 12:00:00 +0700

    fix

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Admin\PastryController;
 
+//get
 Route::get('/', function () {
     return view('welcome');
 });
