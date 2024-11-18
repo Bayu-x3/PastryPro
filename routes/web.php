1c955e9776c847fe13dedfde541db82d33f440a2
Bayu-x3 <rpsmaster12@gmail.com>
2024-11-18 12:00:00 +0700

    chore

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Admin\PastryController;
 
+
 Route::get('/', function () {
     return view('welcome');
 });
